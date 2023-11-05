7f3a03034655283ed93141cd962e290b810ae3a4
Qqkyu <kulmisiu007@gmail.com>
2023-11-05 13:02:20 +0100

    Remove split movements remaps

--- lua/qqkyu/remap.lua
+++ lua/qqkyu/remap.lua
@@ -4,12 +4,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw file explorer
 -- Write file to disk
 vim.keymap.set("n", "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer" })
 
--- Movement
-vim.keymap.set("n", "<leader>h", "<Cmd>wincmd h<CR>", { desc = "Move cursor to left window" })
-vim.keymap.set("n", "<leader>j", "<Cmd>wincmd j<CR>", { desc = "Move cursor to bottomw window" })
-vim.keymap.set("n", "<leader>k", "<Cmd>wincmd k<CR>", { desc = "Move cursor to top window" })
-vim.keymap.set("n", "<leader>l", "<Cmd>wincmd l<CR>", { desc = "Move cursor to right window" })
-
 -- Dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
