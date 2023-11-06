82ec75848259c97917d8f4706ffd163b464c49fa
Qqkyu <kulmisiu007@gmail.com>
2023-11-06 19:54:11 +0100

    Move vim-fugitive and vim-rhubarh to a separate dir

--- init.lua
+++ init.lua
@@ -8,10 +8,6 @@ require("qqkyu")
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
