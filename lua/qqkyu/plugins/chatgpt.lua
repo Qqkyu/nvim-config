dfdf044ab272ee4e96dd871283b3e9429fa7c591
Qqkyu <kulmisiu007@gmail.com>
2023-11-13 21:03:09 +0100

    Setup chat gippity

---
+++ lua/qqkyu/plugins/chatgpt.lua
@@ -0,0 +1,16 @@
+return { {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    local home = vim.fn.expand("$HOME")
+    require("chatgpt").setup({
+      api_key_cmd = "gpg --decrypt " .. home .. "/openai-api-key.txt.gpg",
+      openai_params = { model = "gpt-4", temperature = 0.2 },
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+} }
