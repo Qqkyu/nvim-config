76e2af01b69d3ec6f076a0fe8221d615697eb56b
Qqkyu <kulmisiu007@gmail.com>
2023-11-26 16:42:21 +0100

    Use GPT-4 Turbo version

--- lua/qqkyu/plugins/chatgpt.lua
+++ lua/qqkyu/plugins/chatgpt.lua
@@ -5,7 +5,7 @@ return { {
     local home = vim.fn.expand("$HOME")
     require("chatgpt").setup({
       api_key_cmd = "gpg --decrypt " .. home .. "/openai-api-key.txt.gpg",
-      openai_params = { model = "gpt-4", temperature = 0.2 },
+      openai_params = { model = "gpt-4-1106-preview", temperature = 0.2 },
     })
   end,
   dependencies = {
