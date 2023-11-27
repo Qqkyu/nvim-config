cd2afe85f934f47829e6f2bf9e1f9d1f4255a7b2
Qqkyu <kulmisiu007@gmail.com>
2023-11-27 21:34:56 +0100

    Remaps

--- lua/qqkyu/remap.lua
+++ lua/qqkyu/remap.lua
@@ -4,6 +4,19 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw file explorer
 -- Copy to clipboard
 vim.keymap.set("v", "<C-c>", [["+y]])
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- Write file to disk
 vim.keymap.set("n", "<C-S>", "<Cmd>write<CR>", { desc = "Save buffer" })
 
@@ -17,4 +30,3 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("conform").format({ bufnr = args.buf })
   end,
 })
-
