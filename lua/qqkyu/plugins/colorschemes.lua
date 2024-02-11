8a45bcefd78c0eb136c6b0f362d6cdbe8b756d9a
Qqkyu <kulmisiu007@gmail.com>
2024-02-11 12:07:43 +0100

    Update colorschemes

--- lua/qqkyu/plugins/colorschemes.lua
+++ lua/qqkyu/plugins/colorschemes.lua
@@ -3,11 +3,12 @@ return {
     'rose-pine/neovim',
     config = function()
       require("rose-pine").setup({
-        disable_background = true
+        styles = {
+          transparency = true,
+        }
       })
 
       vim.cmd("colorscheme rose-pine");
     end
   },
 }
-
