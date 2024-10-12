7833eff795883caebe7fe87fc1cc53bf60e20df9
Qqkyu <kulmisiu007@gmail.com>
2024-10-12 19:00:24 +0200

    Enable icons in lualine

--- lua/qqkyu/plugins/lualine.lua
+++ lua/qqkyu/plugins/lualine.lua
@@ -3,10 +3,8 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
       },
       sections = {
         lualine_x = {},
