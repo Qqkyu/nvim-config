7c39173412cf547be850bec37ccadd5a8b512d47
Qqkyu <kulmisiu007@gmail.com>
2025-08-30 20:47:18 +0200

    Update mason.nvim and mason-lspconfig.nvim

--- lua/qqkyu/plugins/lsp.lua
+++ lua/qqkyu/plugins/lsp.lua
@@ -54,8 +54,10 @@ return {
         end, { desc = 'Format current buffer with LSP' })
       end
 
-      require('mason').setup()
-      require('mason-lspconfig').setup()
+      require('neodev').setup()
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
       local servers = {
         lua_ls = {
@@ -66,26 +68,21 @@ return {
         },
       }
 
-      require('neodev').setup()
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      for server_name, _ in pairs(servers) do
+        vim.lsp.config[server_name] = {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        }
+      end
 
-      local mason_lspconfig = require 'mason-lspconfig'
-
-      mason_lspconfig.setup {
+      require('mason').setup()
+      require('mason-lspconfig').setup {
         ensure_installed = vim.tbl_keys(servers),
-      }
-
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          }
-        end,
       }
 
       local cmp = require 'cmp'
